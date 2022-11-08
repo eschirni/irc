2df3cbb20eb5fd60cf5311a7bb969886839792c0
rsiebert <rsiebert@student.42heilbronn.de>
2022-11-08 14:52:52 +0100

    added some error prints

--- main.cpp
+++ main.cpp
@@ -4,19 +4,41 @@
 //for tests
 #include <netinet/in.h>
 #include <unistd.h>
-#include <string.h>
+#include <cstring>
+#include <cerrno>
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_in address;
+	struct sockaddr_in	address;
+	int					rc;
 
 	if (argc != 3)
 		return (1);
 	int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		std::cerr << "socket() failed" << std::endl;
+		std::cerr << "errno: " << strerror(errno) << std::endl;
+		return EXIT_FAILURE;
+	}
 	address.sin_family = AF_LOCAL;
-    address.sin_port = htons(7777);
-	bind(sockfd, (struct sockaddr*)&address, sizeof(address));
-	listen(sockfd, 3);
+    address.sin_port = htons(6667);
+	rc = bind(sockfd, (struct sockaddr*)&address, sizeof(address));
+	if (rc < 0)
+	{
+		std::cerr << "bind() failed" << std::endl;
+		std::cerr << "errno: " << strerror(errno) << std::endl;
+		close(sockfd);
+		return EXIT_FAILURE;
+	}
+	rc = listen(sockfd, 3);
+	if (rc < 0)
+	{
+		std::cerr << "listen() failed" << std::endl;
+		std::cerr << "errno: " << strerror(errno) << std::endl;
+		close(sockfd);
+		return EXIT_FAILURE;
+	}
 	int tmp_len = sizeof(address);
 	int new_socket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&tmp_len);
 	char buffer[1024] = { 0 };
@@ -24,5 +46,5 @@ int main(int argc, char **argv)
 	send(new_socket, argv[2], strlen(argv[2]), 0);
 	close(new_socket);
 	shutdown(sockfd, SHUT_RDWR);
-	return (0);
+	return EXIT_SUCCESS;
 }
