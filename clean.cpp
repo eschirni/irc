141a8cd5fcbf18c5bf5d716c5447321ccd223edc
eschirni <joelschi@gmx.de>
2022-11-10 14:19:11 +0100

    added cleanup

--- clean.cpp
+++ clean.cpp
@@ -1,6 +1,11 @@
 #include "irc.hpp"
 
-int	clean_up(t_serv* serv)
+int	clean_up(t_serv* serv) //closes all left open fds
 {
-	return EXIT_SUCCESS;
+	for (int i = 0; i < serv->n_fds; i++)
+	{
+		if (serv->fds[i].fd >= 0)
+			close (serv->fds[i].fd);
+	}
+	return (EXIT_SUCCESS);
 }
