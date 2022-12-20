996c138d41072904cfbe731083498605911d2fb5
CptBoy <romansiebert0660@gmail.com>
2022-12-20 18:30:15 +0100

    refactored user.hpp for readability

--- include/user.hpp
+++ include/user.hpp
@@ -15,42 +15,55 @@ class User
 	typedef typename std::map<int, User>::iterator mapite_t;
 
 	private:
+		/********************** PIRVATE METHODS ************************/
+
+		void		remove_line(int time = 1);
+		void		send_all(std::string msg);
+		bool		check_nickname(std::string nick);
 		int			initiate_handshake(std::string msg);
 		int			process_handshake(void);
-		void		remove_line(int time = 1);
 		int			send_welcome_reply(void);
 		int			get_current_command(void);
-		bool		check_nickname(std::string nick);
-		int			info(void);
 		mapite_t	get_user(std::string nick);
-		void		send_all(std::string msg);
-		/* cmds */
+
+
+		/************************ COMMANDS *****************************/
+
+		int			info(void);
 		void 		oper(std::string nick, std::string pwd);
 		void		nick(const std::string nick);
 		void		ping(std::string msg);
+
+
+		/********************** DATA-MEMBERS ***************************/
 
 		const int	_fd;
 		t_serv		*_serv;
 		bool		_first_msg;
 		bool		_approved;
-		bool		_is_oper; //idk if server op is okay
+		bool		_is_oper;
+		int			_user_mode;
 		std::string	_client_msg;
 		std::string	_nick_name;
 		std::string	_user_name;
 		std::string	_real_name;
-		int			_user_mode;
 
 	public:
+		/********************** CONSTRUCTION ***************************/
+
 		User(int fd, t_serv *serv);
 		~User(void);
 
+
+		/********************** PUBLIC METHODS *************************/
+
 		std::string	getClientMsg(void) const;
+		std::string	getNickName(void) const;
 		bool		getApproved(void) const;
-		void		setApproved(bool approval);
-		int			process_msg(void);
 		bool		getFirstMsg(void) const;
 		int			getFd(void) const;
-		std::string	getNickName(void) const;
+		void		setApproved(bool approval);
+		int			process_msg(void);
 };
 
 
